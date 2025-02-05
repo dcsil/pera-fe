fd13952dd1e71fb5b4c2f685c4a1f4f50d254115
Tony Juntao Hu <54758881+tonyhu-x@users.noreply.github.com>
2025-02-05 15:24:17 -0500

    Add ESLint Stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,26 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import stylistic from "@stylistic/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    stylistic.configs.customize({
+        flat: true,
+        indent: 4,
+        quotes: "double",
+        semi: true,
+    }),
+    {
+        ignores: [".next/"],
+    },
 ];
 
 export default eslintConfig;
